cf2940cc412c5b2112e5525160829adf938a2899
ShokulSet <104113257+ShokulSet@users.noreply.github.com>
2024-09-02 01:48:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    
+    <h1 class="text-4xl">
+      Hello World
+    </h1>
+    <p class="font-extralight italic">
+      (Insert random cool quotes.)
+    </p>
+  </div>
+</template>
+
+<style>
+
+</style>
