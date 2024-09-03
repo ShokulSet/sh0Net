74c6c19010de3bc3f73b28e7bb742d878cf9aefc
ShokulSet <104113257+ShokulSet@users.noreply.github.com>
2024-09-03 13:30:13 +0700

    02.09.24

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
-
+import { ref, onMounted, onBeforeUnmount } from "vue";
 </script>
 
 <template>
+  <p class="text-center text-3xl mt-2 mb-2">
+    Settapun Laoaree
+  </p>
+  <hr class="w-11/12 m-auto">
   <div class="flex flex-col justify-center items-center h-screen">
-    
-    <h1 class="text-4xl">
-      Hello World
-    </h1>
-    <p class="font-extralight italic">
-      (Insert random cool quotes.)
-    </p>
+    <div class="relative">
+      <p class="text-7xl text-center">
+        Settapun Laoaree
+      </p>
+      <p class="absolute right-0 text-3xl italic font-extralight mt-4">
+        (Insert random cool quotes.)
+      </p>
+    </div>
   </div>
 </template>
 
-<style>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
 </style>
